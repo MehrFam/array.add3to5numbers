8e25d73aca92fe4d93e779616f6c895d0e69c0e6
HP <HP@DESKTOP-1F9G2T4>
2023-09-07 15:15:08 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array.add3to5numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int[] num_array;
+                Console.WriteLine("please enter how many numbers you want to enter");
+                int len = Convert.ToInt32(Console.ReadLine());
+                num_array = new int[len];
+                for (int j = 0; j < len; j++)
+                {
+                    Console.WriteLine("please enter number");
+                    num_array[j] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0; i < num_array.Length; i++)
+                {
+                    Console.WriteLine(num_array[i] + 3);
+                }
+                Console.ReadKey();
+
+            }
+    }
+}
